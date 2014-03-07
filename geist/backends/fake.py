f8e3d508fe2a7e86688a45172862104e0d9baf4d
Adam Hodgen <adam.hodgen@thetestpeople.com>
2014-03-07 14:58:12 +0000

    Add GeistFakeBackend for testing

---
+++ geist/backends/fake.py
@@ -0,0 +1,36 @@
+import numpy as np
+from ..core import Location
+
+
+class GeistFakeBackend(object):
+    def __init__(self, h=800, w=600):
+        self.image = np.zeros((h, w, 3))
+        self.locations = [Location(0, 0, h=h, w=w, image=self.image)]
+
+    def create_process(self, command):
+        pass
+
+    def actions_transaction(self):
+        pass
+
+    def capture_locations(self):
+        for loc in self.locations:
+            yield loc
+
+    def key_down(self, name):
+        pass
+
+    def key_up(self, name):
+        pass
+
+    def button_down(self, button_num):
+        pass
+
+    def button_up(self, button_num):
+        pass
+
+    def move(self, point):
+        pass
+
+    def close(self):
+        pass
