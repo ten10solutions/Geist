c18b8228a1dc1755504f4c5c0f836bc33aedf44f
Adam Hodgen <adam.hodgen@thetestpeople.com>
2014-04-14 11:48:47 +0100

    If we don't get a display_num in Xvfb, find a suitable one

--- geist/backends/xvfb.py
+++ geist/backends/xvfb.py
@@ -11,6 +11,9 @@ from ._x11_common import GeistXBase
 from ..core import Location, LocationList
 
 
+XVFB_PATH = '/var/tmp/Xvfb_display%d'
+
+
 class XwdToNumpyReader(object):
     _XWD_HEADER = struct.Struct('>lllllllhhhhhh')
 
@@ -45,12 +48,15 @@ class GeistXvfbBackend(GeistXBase):
     _FB_OFFSET = 3232
 
     def __init__(self, **kwargs):
-        display_num = kwargs.get('display_num', 2)
+        self.display_num = kwargs.get('display_num', None)
         width = kwargs.get('width', 1280)
         height = kwargs.get('height', 1024)
 
-        display = ":%d" % (display_num, )
-        self._display_dir = '/var/tmp/Xvfb_display%d' % (display_num,)
+        if self.display_num is None:
+            self._find_display()
+
+        display = ":%d" % (self.display_num, )
+        self._display_dir = XVFB_PATH % (self.display_num,)
         os.makedirs(self._display_dir, 0700)
         dev_null = open('/dev/null', 'w')
         self._xvfb_proc = subprocess.Popen(
@@ -74,6 +80,14 @@ class GeistXvfbBackend(GeistXBase):
         time.sleep(1)
         GeistXBase.__init__(self, display=display)
         self._xwd_reader = XwdToNumpyReader(fb_filepath)
+
+    def _find_display(self):
+        """
+        Find a usable display, which doesn't have an existing Xvfb file
+        """
+        self.display_num = 2
+        while os.path.isdir(XVFB_PATH % (self.display_num,)):
+            self.display_num += 1
 
     def capture_locations(self):
         image = self._xwd_reader.get_image()
