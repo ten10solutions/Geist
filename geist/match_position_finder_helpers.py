7c9dd308783db18a56a37004d8705cf80037bc1f
Katie Barr <katie.barr@thetestpeople.com>
2014-12-03 15:35:50 +0000

    added new fuzzy template matching functions

---
+++ geist/match_position_finder_helpers.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+
+def find_potential_match_regions(template, transformed_array, method='correlation', raw_tolerance=0.666):
+    """To prevent prohibitively slow calculation of normalisation coefficient at each point in image
+       find potential match points, and normalise these only these.
+       This function uses the definitions of the matching functions to calculate the expected match value
+       and finds positions in the transformed array matching these- normalisation will then eliminate false positives
+    """
+    if method == 'correlation':
+        match_value  = np.sum(template**2) # this will be the value of the match in the
+    elif method == 'squared difference':
+        match_value = 0
+    elif method == 'correlation coefficient':
+        temp_minus_mean = template - np.mean(template)
+        match_value = np.sum(temp_minus_mean**2)
+    else:
+        raise ValueError('Matching method not implemented')
+    condition = ((np.round(transformed_array, decimals=3)>=match_value*raw_tolerance) &
+                 (np.round(transformed_array, decimals=3)<=match_value*(1./raw_tolerance)))
+    return np.transpose(condition.nonzero())# trsnposition and omparison above take most time
+
+
+
+
+# correlation coefficient matches at top left- perfect for tiling
+# correlation matches to bottom right- so requires transformation for tiling
+def get_tiles_at_potential_match_regions(image, template, transformed_array, method='correlation', raw_tolerance=0.001):
+    if method not in ['correlation', 'correlation coefficient', 'squared difference']:
+        raise ValueError('Matching method not implemented')
+    match_points = find_potential_match_regions(template, transformed_array, method=method, raw_tolerance=raw_tolerance)
+    h, w = template.shape
+    # now only need to get tiles and normalisation coefficients at these positions
+    if method in 'correlation':
+        # BUG HERE!! gets negative values
+        match_points = [(match[0]-h+1, match[1] - w +1) for match in match_points]
+    else:
+        match_points = [(match[0], match[1]) for match in match_points]
+    match_point_dict = {i:point for (i, point) in enumerate(match_points)}
+    # create tile for each match point- use dict so we know which match point it applies to
+    # match point here is position of top left pixel of tile
+    image_tiles_dict = {match_points[i]:image[match_points[i][0]:match_points[i][0]+h,match_points[i][1]:match_points[i][1]+w] for i in range(len(match_points))}
+    return image_tiles_dict
+
+
+
+###############################################
+
+# image tiles dict is of form match_point coord:tile at that point
+def normalise_correlation(image_tile_dict, transformed_array, template, normed_tolerance=1):
+    """Calculates the normalisation coefficients of potential match positions
+       Then normalises the correlation at these positions, and returns them
+       if they do indeed constitute a match
+    """
+    template_norm = np.linalg.norm(template)
+    image_norms = {(x,y):np.linalg.norm(image_tile_dict[(x,y)])*template_norm for (x,y) in image_tile_dict.keys()}
+    match_points = image_tile_dict.keys()
+    # for correlation, then need to transofrm back to get correct value for division
+    h, w = template.shape
+    points_from_transformed_array = [(match[0] + h - 1, match[1] + w - 1) for match in match_points]
+    image_matches_normalised = {points_from_transformed_array[i]:transformed_array[points_from_transformed_array[i][0], points_from_transformed_array[i][1]]/image_norms[match_points[i]] for i in range(len(match_points))}
+    result = {key:value for key, value in image_matches_normalised.items() if np.round(value, decimals=3) >= normed_tolerance}
+    return result.keys()
+
+
+# image tiles dict is of form match_point coord:tile at that point
+def normalise_correlation_coefficient(image_tile_dict, transformed_array, template, normed_tolerance=1):
+    """As above, but for when the correlation coefficient matching method is used
+    """
+    template_mean = np.mean(template)
+    template_minus_mean = template - template_mean
+    template_norm = np.linalg.norm(template_minus_mean)
+    image_norms = {(x,y):np.linalg.norm(image_tile_dict[(x,y)]- np.mean(image_tile_dict[(x,y)]))*template_norm for (x,y) in image_tile_dict.keys()}
+    match_points = image_tile_dict.keys()
+    # for correlation, then need to transofrm back to get correct value for division
+    h, w = template.shape
+    image_matches_normalised = {match_points[i]:transformed_array[match_points[i][0], match_points[i][1]]/image_norms[match_points[i]] for i in range(len(match_points))}
+    normalised_matches = {key:value for key, value in image_matches_normalised.items() if np.round(value, decimals=3) >= normed_tolerance}
+    return normalised_matches.keys()
+
+
---
+++ geist/matchers.py
@@ -0,0 +1,139 @@
+from .match_position_finder_helpers import get_tiles_at_potential_match_regions, normalise_correlation, normalise_correlation_coefficient
+from scipy.signal import fftconvolve
+from scipy.ndimage.measurements import label, find_objects
+
+# both these methods return array of points giving bottom right coordinate of match
+
+def match_via_correlation(image, template, raw_tolerance=1, normed_tolerance=0.9):
+    """ Matchihng algorithm based on normalised cross correlation.
+        Using this matching prevents false positives occuring for bright patches in the image
+    """
+    # fft based convolution enables fast matching of large images
+    correlation = fftconvolve(image, template[::-1,::-1])
+    # find images regions which are potentially matches
+    match_position_dict = get_tiles_at_potential_match_regions(image, template, correlation, raw_tolerance=raw_tolerance)
+    # bright spots in images can lead to false positivies- the normalisation carried out here eliminates those
+    results = normalise_correlation(match_position_dict, correlation, template, normed_tolerance=normed_tolerance)
+    return results
+
+
+
+
+def match_via_correlation_coefficient(image, template, raw_tolerance=1, normed_tolerance=0.9):
+    """ Matching algorithm based on 2-dimensional version of Pearson product-moment correlation coefficient.
+
+        This is more robust in the case where the match might be scaled or slightly rotated.
+
+        From experimentation, this method is less prone to false positives than the correlation method.
+    """
+    temp_mean = np.mean(template)
+    temp_minus_mean = template - temp_mean
+    convolution = fftconvolve(image, temp_minus_mean[::-1,::-1])
+    match_position_dict = get_tiles_at_potential_match_regions(image, template, convolution, method='correlation coefficient', raw_tolerance=raw_tolerance)
+    # this is empty, so think condition is wrong
+    results = normalise_correlation_coefficient(match_position_dict, convolution, template, normed_tolerance=normed_tolerance)
+    return results
+
+
+
+
+def fuzzy_match(image, template, normed_tolerance=None, raw_tolerance=None, method='correlation'):
+    """Determines, using one of two methods, whether a match(es) is present and returns the positions of
+       the bottom right corners of the matches.
+       Fuzzy matches returns regions, so the center of each region is returned as the final match location
+
+       USE THIS METHOD IF you need to match, e.g. the same image but rendered slightly different with respect to
+       anti aliasing; the same image on a number of different backgrounds.
+
+       The raw_tolerance is the proportion of the value at match positions (i.e. the value returned for an exact match)
+       that we count as a match. For fuzzy matching, this value will not be exactly the value returned for an exact match
+       N. B. Lowering raw_tolerance increases the number of potential match tiles requiring normalisation.
+       This DRAMATICALLY slows down matching as normalisation (a process which eliminates false positives)
+
+       The normed_tolerance is how far a potential match value can differ from one after normalisation.
+
+       The tolerance values indicated below are from a short investigation, looking to minimise missing items we wish to match,
+       as all as false positives which inevitably occur when performing fuzzy matching. To generate these values, we
+       tested maching letters with different type of antialiasing on a number of backgrounds.
+    """
+    if method == 'correlation':
+        if not raw_tolerance:
+            raw_tolerance = 0.85
+        if not normed_tolerance:
+            normed_tolerance = 0.85
+        results = np.array(match_via_correlation(image, template, raw_tolerance=raw_tolerance, normed_tolerance=normed_tolerance))
+    elif method == 'correlation coefficient':
+        if not raw_tolerance:
+            raw_tolerance = 0.8
+        if not normed_tolerance:
+            normed_tolerance = 0.9
+        results = np.array(match_via_correlation_coefficient(image, template, raw_tolerance=raw_tolerance, normed_tolerance=normed_tolerance))
+    h, w = image.shape
+    th, tw = template.shape
+    results = np.array([[result[0], result[1]] for result in results if (th < result[0] < h and tw < result[1] < w)])
+    #match_x, match_y = int(np.mean(results[:,1])), int(np.mean(results[:,0]))
+    results_aggregated_mean_match_position = match_positions((h,w), results)
+    return results_aggregated_mean_match_position
+
+    #return results
+
+
+def match_positions(shape, list_of_coords):
+    """ In cases where we have multiple matches, each highlighted by a region of coordinates,
+        we need to separate matches, and find mean of each to return as match position
+    """
+    match_array = np.zeros(shape)
+    try:
+        # excpetion hit on this line if nothing in list_of_coords- i.e. no matches
+        match_array[list_of_coords[:,0],list_of_coords[:,1]] = 1
+        labelled = label(match_array)
+        objects = find_objects(labelled[0])
+        coords = [{'x':(slice_x.start, slice_x.stop),'y':(slice_y.start, slice_y.stop)} for (slice_y,slice_x) in objects]
+        final_positions = [(int(np.mean(coords[i]['y'])),int(np.mean(coords[i]['x']))) for i in range(len(coords))]
+        return final_positions
+    except IndexError:
+        print 'no matches found'
+        # this error occurs if no matches are found
+        return []
+
+
+## not what we want a all!!! only will take exact matches, defeating entire point
+def post_process(image, template, list_of_coords):
+    h, w = template.shape
+    for x, y in list_of_coords:
+        print x-h + 1, y-w + 1
+        sub_image = image[x-h + 1:x + 1, y-w + 1:y + 1]
+        print sub_image.shape, template.shape, x, y
+        if not np.allclose(template, sub_image):
+            list_of_coords.remove((x,y))
+    return list_of_coords
+
+
+def to_rgb(im):
+    return np.dstack([im.astype(np.uint8)] * 3).copy(order='C')
+
+
+def highlight_matched_region_no_normalisation(image, template, method='correlation', normed_tolerance=0.666):
+    conv = fftconvolve(image, template[::-1,::-1])
+    th, tw = template.shape
+    r = find_potential_match_regions(template, conv, method=method, normed_tolerance=normed_tolerance)
+    r_in_image = [(r_x, r_y) for (r_x, r_y) in r if (r_x < image.shape[0] and r_y < image.shape[1])]
+    im_rgb = to_rgb(image)
+    for (x,y) in r_in_image:
+        try:
+            im_rgb[x-th:x,y-tw:y] = 0, 100, 100
+        except IndexError:
+            im_rgb[x,y] = 0, 100, 100
+    return im_rgb
+
+
+def highlight_matched_region_normalised(image, shape, list_of_coords):
+    th, tw = shape
+    im_rgb = to_rgb(image)
+    for (x,y) in list_of_coords:
+        #print (x,y)
+        try:
+            im_rgb[x-th:x,y-tw:y] = 0, 100, 100
+        except IndexError:
+            im_rgb[x,y] = 0, 100, 100
+    return im_rgb
