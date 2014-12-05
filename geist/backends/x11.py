12b62a8badd5544cf7f3e9cd981762f7e9d5abe6
Katie Barr <katie.barr@thetestpeople.com>
2014-12-05 10:26:17 +0000

    added wait to Popen

--- geist/backends/x11.py
+++ geist/backends/x11.py
@@ -36,7 +36,7 @@ class GeistXBackend(GeistXBase):
         geometry_getter = attrgetter('x', 'y', 'width', 'height')
         x, y, w, h = geometry_getter(self._root.get_geometry().reply())
         #res = np.array(ImageGrab.grab())
-        subprocess.Popen("import -window root screenshot.png", shell=True)
+        subprocess.Popen("import -window root screenshot.png", shell=True).wait()
         res = np.array(Image.open('screenshot.png'))
         # remove once we have loaded the image as a numpy array
         subprocess.Popen("rm screenshot.png",shell=True)
