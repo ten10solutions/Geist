2ccb1af5df69704c601a76fa5ab0996c5b97385f
Jordan <jordan.brennan@thetestpeople.com>
2015-09-29 14:50:59 +0100

    Added an example prereq

---
+++ prereq.py
@@ -0,0 +1,32 @@
+"""
+Run this script with ipython -i prereq.py
+This starts ipython in interactive mode.
+
+Use the viewer object to capture images and test finders in an interactive ipython
+"""
+
+try:
+    get_ipython().magic(u'pylab')
+except (NameError,):
+    pass
+
+from geist.backends import get_platform_backend
+
+from geist import (
+    GUI,
+    DirectoryRepo,
+)
+
+from geist.pyplot import Viewer
+
+# Mark the directory listed as the current image repository
+repo = DirectoryRepo(repo_location)
+
+# Start a Geist backend, takes an optional screen number to capture.
+backend = get_platform_backend()
+
+# Start a Geist GUI
+gui = GUI(backend)
+
+# Viewer  object can be used to display results in image form
+viewer = Viewer(login_page.gui, login_page.repo)
